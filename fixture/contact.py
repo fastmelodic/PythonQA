30773447a23f0a5a536fed400fa0197ceb516e46
Tim <l2guns1cowboyl@gmail.com>
2019-11-30 16:52:20 +0300

    added all operations for contacts

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,13 +1,20 @@
-from selenium.webdriver.support.ui import Select\
+from selenium.webdriver.support.ui import Select
 
 class ContactHelper:
 
     def __init__(self, app):
         self.app = app
 
-    def fill_form(self, Contact):
-        wd = self.app.wd
+    def fill_form(self, Contact, Date1, Date2):
         self.open_page_create_contact()
+        self.fill_main_info(Contact)
+        self.fill_bday(Date1)
+        self.fill_aday(Date2)
+        self.fill_address()
+        self.submit()
+
+    def fill_main_info(self, Contact):
+        wd = self.app.wd
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(Contact.firstname)
@@ -29,30 +36,27 @@ class ContactHelper:
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys(Contact.email)
-        self.fill_bday()
-        self.fill_aday()
-        self.fill_address()
-        self.submit()
 
-    def fill_bday(self):
+
+    def fill_bday(self, Date):
         wd = self.app.wd
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("3")
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(Date.day)
         wd.find_element_by_name("bday").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("February")
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(Date.month)
         wd.find_element_by_name("bmonth").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys("1990")
 
-    def fill_aday(self):
+    def fill_aday(self,Date):
         wd = self.app.wd
         wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text("4")
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(Date.day)
         wd.find_element_by_name("aday").click()
         wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text("March")
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(Date.month)
         wd.find_element_by_name("amonth").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
@@ -71,3 +75,20 @@ class ContactHelper:
     def open_page_create_contact(self):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
+
+    def delete_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
+
+    def edit_contact(self, Contact, Date1, Date2):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        self.fill_main_info(Contact)
+        self.fill_bday(Date1)
+        self.fill_aday(Date2)
+        self.fill_address()
+        wd.find_element_by_name("update").click()
+        wd.find_element_by_link_text("home page").click()
+
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+from model.date import  Date
 
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.fill_form(Contact(firstname = "tim", middlename = "Share", lastname = "L", nickname = "popa", title = "RAZ", company = "TANDIR", email = "dpd@mail.com"))
+    app.contact.fill_form(Contact(firstname = "tim", middlename = "Share", lastname = "L", nickname = "popa", title = "RAZ", company = "TANDIR", email = "dpd@mail.com"), Date("20", "March"), Date("20", "June"))
     app.session.logout()
 
 
