8d2d930cbc599a0075570eceb3da1ec4c407a5a3
Tim <l2guns1cowboyl@gmail.com>
2019-12-17 21:16:14 +0300

    added target-config file in .json

--- conftest.py
+++ conftest.py
@@ -1,14 +1,23 @@
 from fixture.application import Application
 import pytest
+import json
+import os.path
 
 fixture = None
+target = None
 
 @pytest.fixture
-def app():
+def app(request):
     global fixture
+    global target
+    browser = request.config.getoption("--browser")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
     if fixture is None or not fixture.is_valid():
-        fixture = Application()
-    fixture.session.ensure_login("admin", "secret")
+        fixture = Application(browser, target["baseUrl"])
+    fixture.session.ensure_login(target["username"], target["password"])
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
@@ -18,3 +27,7 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action = "store", default = "chrome")
+    parser.addoption("--target", action = "store", default = "target.json")
--- fixture/group.py
+++ fixture/group.py
@@ -39,7 +39,7 @@ class GroupHelper:
 
     def open_group_page(self):
         wd = self.app.wd
-        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("name")) > 0):
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
             wd.find_element_by_link_text("groups").click()
 
     def delete_first_group(self):
