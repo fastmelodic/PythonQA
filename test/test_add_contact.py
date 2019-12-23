1eb97f164b50e048fe55e2e2058e01706451a794
Tim <l2guns1cowboyl@gmail.com>
2019-12-23 20:43:26 +0300

    updated tests for better check_UI

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,10 +11,8 @@ def test_add_contact(app, json_contacts, db, check_ui):
     new_contacts = db.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-    def clean(contact):
-        return Contact(id=contact.id, firstname=contact.firstname.strip(), lastname=contact.lastname.strip())
     if check_ui:
-        new_contacts = map(clean, db.get_contact_list())
+        new_contacts = map(lambda x: Contact(id=contact.id, firstname=x.firstname.strip(), lastname=x.lastname.strip()), db.get_contact_list())
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 
 
